21ca5d62a2de88eb7178e2c36b4ab066be52f597
Anacleto Mimoso <anacleto.mimoso@bancobai.ao>
2022-02-13 09:06:22 +0100

    create person routes

--- src/app/person/person-routing.module.ts
+++ src/app/person/person-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonIndexComponent } from './person-index/person-index.component';
+import { PersonShowComponent } from './person-show/person-show.component';
 
 const routes: Routes = [
-  { path: '', component: PersonIndexComponent }
+  { path: '', redirectTo: 'person' },
+  // { path: '', component: PersonIndexComponent },
+  {
+    path: 'person',
+    component: PersonIndexComponent
+  },
+  {
+    path: 'person/add',
+    component: PersonShowComponent
+  },
+  {
+    path: 'person/show',
+    component: PersonShowComponent
+  },
+  {
+    path: 'person/edit',
+    component: PersonShowComponent
+  },
+  {
+    path: 'person/delete',
+    component: PersonShowComponent
+  }
 ];
 
 @NgModule({
